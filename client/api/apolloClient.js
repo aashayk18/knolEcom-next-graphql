8705e8abacd9008caea692f96e4ab0b7f2bb2198
aashayk18 <aashayk100@gmail.com>
2024-04-01 12:16:05 +0530

    graphql url update

--- client/api/apolloClient.js
+++ client/api/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://knol-ecom-graphql-server.vercel.app/graphql',
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
