f6c29700f77e15781f5dcd03b102c42bf4b8d119
aashayk18 <aashayk100@gmail.com>
2024-04-01 14:21:49 +0530

    dockerize backend

--- server/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { fetchProducts, getProduct, getProductIds } = require('../controllers/productController');
-
-router.get('/products', fetchProducts);
-router.get('/products/:id', getProduct);
-router.get('/product/ids', getProductIds)
-
-module.exports = router;
